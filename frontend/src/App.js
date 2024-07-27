75520cb4fe18f1012542d9da59e25fa46305887f
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2024-07-27 02:18:11 +0200

    Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/HomePage'
+import AuthPage from './pages/AuthPage'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/auth' element={<AuthPage />} />
+        <Route index element={<Home />} />
+        <Route path='*' element = {<>Page not found</>} />
+      </Routes>
     </div>
   );
 }
